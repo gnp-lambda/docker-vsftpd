298b3999d9b3efa879ed8e1e04ea9251347c8d86
Gabriel Parrondo <gabriel.parrondo@lambdasistemas.com.ar>
2019-10-01 15:15:33 -0300

    Fix log output and allow user defined virtual_user database

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -17,19 +17,22 @@ else
     export LOG_STDOUT='Yes.'
 fi
 
-# Create home dir and update vsftpd user db:
+# Create home dir
 mkdir -p "/home/vsftpd/${FTP_USER}"
 chown -R ftp:ftp /home/vsftpd/
 
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
+# Update vsftpd user db:
+if ! grep -q "^${FTP_USER}$" /etc/vsftpd/virtual_users.txt > /dev/null 2>&1; then
+    echo -e "${FTP_USER}\n${FTP_PASS}" >> /etc/vsftpd/virtual_users.txt
+else
+    echo "WARNING: Ignoring FTP_USER because it already exists in virtual_users.txt" >&2
+fi
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
 # Set passive mode parameters:
-if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+if [ "$PASV_ADDRESS" != "**IPv4**" ]; then
+    echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
 fi
-
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
@@ -39,7 +42,7 @@ echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
 echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
 
 # Get log file path
-export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
+export LOG_FILE=/var/log/vsftpd.log
 
 # stdout server info:
 if [ ! $LOG_STDOUT ]; then
@@ -59,8 +62,9 @@ cat << EOB
 	· Redirect vsftpd log to STDOUT: No.
 EOB
 else
-    /usr/bin/ln -sf /dev/stdout $LOG_FILE
+    touch "$LOG_FILE"
+    tail --pid $$ -n0 -F $LOG_FILE &
 fi
 
 # Run vsftpd:
-&>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
